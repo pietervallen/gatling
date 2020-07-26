b37101423972838c8795002205ab84c72fca24df
Pieter Vallen <Pieter@Vallen.net>
2020-07-26 12:24:22 +0200

    Created project using archetype, copied new simulation and ran mvn gatling:test

---
+++ src/test/scala/UserMsSimulation.scala
@@ -0,0 +1,34 @@
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+
+import scala.concurrent.duration._
+
+class UserMsSimulation extends Simulation {
+
+  val httpProtocol = http
+    .baseUrl("http://localhost:8182")
+    .inferHtmlResources(BlackList(""".*\.css""", """.*\.js""", """.*\.ico"""), WhiteList())
+    .acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
+    .acceptEncodingHeader("gzip, deflate")
+    .acceptLanguageHeader("it-IT,it;q=0.8,en-US;q=0.5,en;q=0.3")
+    .userAgentHeader("Mozilla/5.0 (Windows NT 10.0; WOW64; rv:46.0) Gecko/20100101 Firefox/46.0")
+
+  val scn = scenario("Users Scenario")
+    .exec(http("Get Users")
+      .get("/user"))
+
+  val testScenario = scenario("Test Scenario")
+    .exec(http("Get Users")
+      .get("/user/1")
+      .check(jsonPath("$.email").saveAs("email")))
+    .exec { session =>
+      // displays the content of the session in the console (debugging only)
+      println("DEBUG - email: " + session("email").as[String])
+      // return the original session
+      session
+    }
+
+  setUp(testScenario.inject(
+    atOnceUsers(1))
+      .protocols(httpProtocol))
+}
